d6ea3e911670f698334865ed8af738b4ef8f5e38
Vishwajeet Kondi <vishwajeet.k@maybank.com>
2021-02-25 14:22:56 +0800

    fixed untracked files

--- ios/Pods/Target Support Files/Pods-BMExpenses-BMExpensesTests/Pods-BMExpenses-BMExpensesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BMExpenses_BMExpensesTests : NSObject
-@end
-@implementation PodsDummy_Pods_BMExpenses_BMExpensesTests
-@end
--- ios/Pods/Target Support Files/Pods-BMExpenses/Pods-BMExpenses-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BMExpenses : NSObject
-@end
-@implementation PodsDummy_Pods_BMExpenses
-@end
