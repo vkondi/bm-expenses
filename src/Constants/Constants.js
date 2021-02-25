b56e1b83b08dffac18865ec12216650609234227
Vishwajeet Kondi <vishwajeet.k@maybank.com>
2021-02-25 16:42:55 +0800

    Introduced Redux For Global State Management.

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -10,3 +10,7 @@ export const CATG_SHOPPING = 'Shopping';
 export const CATG_FOOD = 'Food';
 export const CATG_UTILITY = 'Utilities';
 export const CATG_HOLIDAY = 'Holidays';
+
+// Redux Action Types
+export const SET_MAIN_DATA = 'SET_MAIN_DATA';
+export const SET_FILTER_VALUE = 'SET_FILTER_VALUE';
